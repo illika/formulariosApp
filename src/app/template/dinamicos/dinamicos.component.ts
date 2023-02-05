8e00f9984615e11cbd1b353d6ba5996578d64539
illika <steve.milan7@gmail.com>
2023-02-04 23:51:37 -0500

    DInamico - extra

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +17,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  @ViewChild("miForm") miForm!: NgForm;
 
-  ngOnInit(): void {
+  nuevoJuego: string = "";
+
+  persona: Persona = {
+    nombre: "Illika",
+    favoritos: [
+      { id: 1, nombre: "Dota 2" },
+      { id: 2, nombre: "WOW" }
+    ]
+  };
+
+
+  guardar() {
+    console.log(this.miForm);
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarFav() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = "";
+
   }
 
 }
