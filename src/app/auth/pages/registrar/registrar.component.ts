92b1f0d5d15048e9b00c79ce8005728ae7f70147
illika <steve.milan7@gmail.com>
2023-02-06 15:18:34 -0500

    Validators Email

--- src/app/auth/pages/registrar/registrar.component.ts
+++ src/app/auth/pages/registrar/registrar.component.ts
@@ -11,18 +11,22 @@ export class RegistrarComponent implements OnInit {
 
 
   nombreApellidoPattern: string = "([a-zA-Z]+) ([a-zA-Z]+)";
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   constructor(private fb: FormBuilder) { }
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ["", [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
-    email: ["", [Validators.required]],
+    email: ["", [Validators.required, Validators.pattern(this.emailPattern)]],
     username: ["", [Validators.required]],
     password: ["", [Validators.required]],
   });
 
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: "Illika Pa",      
+    })
   }
 
   campoNoValido(campo: string) {
