d89b44b60c1a935d2113d8572497cc99e6948bd7
illika <steve.milan7@gmail.com>
2023-02-06 15:38:25 -0500

    Validastors personalizado(FormControl)

--- src/app/auth/pages/registrar/registrar.component.ts
+++ src/app/auth/pages/registrar/registrar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -13,19 +13,32 @@ export class RegistrarComponent implements OnInit {
   nombreApellidoPattern: string = "([a-zA-Z]+) ([a-zA-Z]+)";
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  noUsername(control: FormControl) {
+    const valor = control.value?.trim().toLowerCase();
+    if (valor === 'illika') {
+      return {
+        noIllika: true
+      }
+    }
+    return null;
+
+  }
+
   constructor(private fb: FormBuilder) { }
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ["", [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
     email: ["", [Validators.required, Validators.pattern(this.emailPattern)]],
-    username: ["", [Validators.required]],
+    username: ["", [Validators.required, this.noUsername]],
     password: ["", [Validators.required]],
   });
 
 
   ngOnInit(): void {
     this.miFormulario.reset({
-      nombre: "Illika Pa",      
+      nombre: "Illika Pa",
+      email: "test@email.com",
+      username: 'katari'
     })
   }
 
@@ -34,11 +47,9 @@ export class RegistrarComponent implements OnInit {
   }
 
   grabar() {
-    if(this.miFormulario.invalid) return this.miFormulario.markAllAsTouched();
+    if (this.miFormulario.invalid) return this.miFormulario.markAllAsTouched();
 
     console.log("grabar")
-    
-    
   }
 
 }
