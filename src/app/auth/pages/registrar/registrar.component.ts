10e02641bd3d32486e1cdcf6f1ba8a090baad87d
illika <steve.milan7@gmail.com>
2023-02-06 17:41:56 -0500

    Validacion asincrona

--- src/app/auth/pages/registrar/registrar.component.ts
+++ src/app/auth/pages/registrar/registrar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 import { emailPattern, nombreApellidoPattern, noUsername } from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
@@ -12,11 +13,12 @@ import { ValidatorService } from 'src/app/shared/validator/validator.service';
 export class RegistrarComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
-    private validatorService: ValidatorService) { }
+    private validatorService: ValidatorService,
+    private emailValidator: EmailValidatorService) { }
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ["", [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
-    email: ["", [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    email: ["", [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
     username: ["", [Validators.required, this.validatorService.noUsername]],
     password: ["", [Validators.required, Validators.minLength(3)]],
     password2: ["", [Validators.required]],
@@ -28,7 +30,7 @@ export class RegistrarComponent implements OnInit {
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: "Illika Pa",
-      email: "test@email.com",
+      email: "test1@test.com",
       username: 'katari'
     })
   }
